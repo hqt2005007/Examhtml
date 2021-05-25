234c357509aa800ba6c81f7d2c779da34758091c
hqt2005007 <hqt2005007@gmail.com>
2021-05-25 22:32:34 +0700

    Exam

---
+++ Ex5.js
@@ -0,0 +1,11 @@
+function check() {
+
+    var textLength = document.getElementById('textarea').value.length;
+
+    if(textLength >200)
+    {
+        alert("Nhập quá giới hạn");
+    }
+    alert("OK");
+    
+}
